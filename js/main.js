d1c7b6373a5a9db1bf9e0f60f95875573884317b
Ahmed Naguib <46289250+AhmedNaguib-Freelancer@users.noreply.github.com>
2019-09-04 21:42:21 +0200

    js
    js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿var elem = document.getElementById("myvideo");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
